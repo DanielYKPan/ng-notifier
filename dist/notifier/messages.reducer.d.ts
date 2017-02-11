020302df59d11d797757490034333b21d24ab253
Yaokun Pan <myron.yk.pan@gmail.com>
2017-02-11 21:20:26 +1100

    publish beta10

--- dist/notifier/messages.reducer.d.ts
+++
@@ -1,6 +0,0 @@
-import { ActionReducer } from "@ngrx/store";
-import { INotifierMessage } from "./notifier.model";
-export declare const ADD_MESSAGE: string;
-export declare const REMOVE_MESSAGE: string;
-export declare const REMOVE_ALL: string;
-export declare const messagesReducer: ActionReducer<Array<INotifierMessage>>;
