65d1d4ca0b404976214b3ea95ec72b4efc335706
Yaokun Pan <myron.yk.pan@gmail.com>
2017-02-18 15:33:15 +1100

    Used beta-12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,6 +28,6 @@ export class AppComponent implements OnInit {
     }
 
     addOverride() {
-        this.service.info('hello world override', 'Angular Message', {animate: 'fade', timeDelay: 0});
+        this.service.info('hello world override', 'Angular Message', {animate: 'fade', timeDelay: 0, titleClass: 'titleOverride', messageClass: 'messageOverride'});
     }
 }
